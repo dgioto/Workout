095f968ad995aab333e4bb1379fa8af016eb26fe
Alexander Tih <dgioto@gmail.com>
2021-03-24 13:20:43 +0200

    add fragment

--- app/src/main/java/com/example/workout/WorkoutDetailFragment.java
+++ app/src/main/java/com/example/workout/WorkoutDetailFragment.java
@@ -8,10 +8,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link WorloutDetailFragment#newInstance} factory method to
+ * Use the {@link WorkoutDetailFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class WorloutDetailFragment extends Fragment {
+public class WorkoutDetailFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,7 +22,7 @@ public class WorloutDetailFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public WorloutDetailFragment() {
+    public WorkoutDetailFragment() {
         // Required empty public constructor
     }
 
@@ -35,8 +35,8 @@ public class WorloutDetailFragment extends Fragment {
      * @return A new instance of fragment WorloutDetailFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static WorloutDetailFragment newInstance(String param1, String param2) {
-        WorloutDetailFragment fragment = new WorloutDetailFragment();
+    public static WorkoutDetailFragment newInstance(String param1, String param2) {
+        WorkoutDetailFragment fragment = new WorkoutDetailFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -57,6 +57,6 @@ public class WorloutDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_worlout_detail, container, false);
+        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
     }
 }
