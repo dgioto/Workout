57e32e2a6c03b73cdb9c5a99cfc0e5d10ddc3e84
Alexander Tih <dgioto@gmail.com>
2021-03-28 17:10:36 +0300

    add fragment WorkoutListFragment

---
+++ app/src/main/java/com/example/workout/WorkoutListFragment.java
@@ -0,0 +1,18 @@
+package com.example.workout;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WorkoutListFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
