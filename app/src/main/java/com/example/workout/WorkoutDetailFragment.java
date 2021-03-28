4fe636edbe79975bb4bb06be3e8f63c4f65b1e45
Alexander Tih <dgioto@gmail.com>
2021-03-28 16:34:12 +0300

    запалняем надписи в фрагменте

--- app/src/main/java/com/example/workout/WorkoutDetailFragment.java
+++ app/src/main/java/com/example/workout/WorkoutDetailFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class WorkoutDetailFragment extends Fragment {
@@ -19,6 +20,19 @@ public class WorkoutDetailFragment extends Fragment {
                 R.layout.fragment_workout_detail,
                 container,
                 false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        if (view != null){
+            TextView title = (TextView) view.findViewById(R.id.textTitle);
+            Workout workout = Workout.workouts[(int) workoutId];
+            title.setText(workout.getName());
+            TextView description = (TextView) view.findViewById(R.id.textDescription);
+            description.setText(workout.getDescription());
+        }
     }
 
     public void setWorkoutId(long id){
