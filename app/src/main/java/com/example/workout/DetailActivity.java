58119992941e3108a5bf6a7a21c3ea5c3c89bcfe
Alexander Tih <dgioto@gmail.com>
2021-03-31 23:11:31 +0300

    передача индификатора WorkoutDetailFragment

--- app/src/main/java/com/example/workout/DetailActivity.java
+++ app/src/main/java/com/example/workout/DetailActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 
 public class DetailActivity extends AppCompatActivity {
 
+    //константа для передачи идентификатора из MainActivity в DetailActivity
+    //что бы конкретное значение не фиксировалось в коде
+    public static final String EXTRA_WORKOUT_ID = "id";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,7 +18,10 @@ public class DetailActivity extends AppCompatActivity {
         //В макете активности этому фрагменту присвоен индификатор detail_frag
         WorkoutDetailFragment frag = (WorkoutDetailFragment)
                 getSupportFragmentManager().findFragmentById(R.id.detail_frag);
-        //выводим информацию о произвольно выбранном комплексе
-        frag.setWorkoutId(1);
+//        //выводим информацию о произвольно выбранном комплексе
+//        frag.setWorkoutId(1);
+        //получаем идентификатор из интента и передаем его фграгменту методом setWorkoutId
+        int workoutId = (int) getIntent().getExtras().get(EXTRA_WORKOUT_ID);
+        frag.setWorkoutId(workoutId);
     }
 }
