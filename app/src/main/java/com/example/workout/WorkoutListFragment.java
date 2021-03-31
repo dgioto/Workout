c5b30aa45781f793c14e1e4af9d38cdcc1e31977
Alexander Tih <dgioto@gmail.com>
2021-03-31 22:58:00 +0300

    определяем интерфейс, регистрируем слушатель когда WorkoutListFragment присоединится к ней, сообщаем слушателю, что на варианте был сделан щелчок

--- app/src/main/java/com/example/workout/WorkoutListFragment.java
+++ app/src/main/java/com/example/workout/WorkoutListFragment.java
@@ -1,5 +1,6 @@
 package com.example.workout;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
@@ -7,9 +8,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 //активность рассширяется от класса ListFragment
 public class WorkoutListFragment extends ListFragment {
+
+    //создаем интерфейс слушатель к фрагменту
+    static interface  Listener{
+        void  itemClicked(long id);
+    }
+    //сохраняем ссылку на активность, с которой связывается WorkoutListFragment
+    private Listener listener;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,5 +39,22 @@ public class WorkoutListFragment extends ListFragment {
 
         // вызов метода суер класса предоставляет макет по умолчанию для ListFragment
         return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    //вызывается когда фрагмент связывается с активностью
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.listener = (Listener) context;
+    }
+
+    //Передает слушателю индификатор комплекса упражнений, выбранного пользователя
+    //метод получает 4 параметра:
+    // списковое представление, выбранный вариант списка, его позицию, индификатор записи
+    @Override
+    public void onListItemClick(ListView listView, View itemView, int position, long id) {
+        if (listener != null)
+            //сообщает слушателю,когда пользователь выбрал вариант в ListView
+            listener.itemClicked(id);
     }
 }
