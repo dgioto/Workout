b8c69cc47e243572a01a594ad686296fe684e544
Alexander Tih <dgioto@gmail.com>
2021-04-05 22:36:03 +0300

    транзакции фрагментов

--- app/src/main/java/com/example/workout/MainActivity.java
+++ app/src/main/java/com/example/workout/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.workout;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -25,6 +26,18 @@ public class MainActivity extends AppCompatActivity implements WorkoutListFragme
         View fragmentContainer = findViewById(R.id.fragment_container);
         if (fragmentContainer != null){
             //Добавление фрагмента в FragmentLayout
+            WorkoutDetailFragment details = new WorkoutDetailFragment();
+            //Начало транзакции фрагмента
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            details.setWorkoutId(id);
+            //замена фрагмента
+            ft.replace(R.id.fragment_container, details);
+            //новый и старый фрагменты должны сопровождаться анимацией
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            //транзакция включается в стек возврата
+            ft.addToBackStack(null);
+            //закрепление транзакции
+            ft.commit();
         } else {
             Intent intent = new Intent(this, DetailActivity.class);
             intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int) id);
