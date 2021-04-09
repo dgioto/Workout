3bbdb58eb4f8b5b813cd69998ffa899a88f63353
Alexander Tih <dgioto@gmail.com>
2021-04-09 16:15:53 +0300

    перевод activity_temp.xml на использование FrameLayout, добавление транзакции фрамента в TempActivity

--- app/src/main/java/com/example/workout/TempActivity.java
+++ app/src/main/java/com/example/workout/TempActivity.java
@@ -1,13 +1,30 @@
 package com.example.workout;
 
+import android.annotation.SuppressLint;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class TempActivity extends AppCompatActivity {
 
+    @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp);
+
+        //фрагмент добавляется только, если активность создается заново после уничтожения
+        if(savedInstanceState == null){
+            StopwatchFragment stopwatch = new StopwatchFragment();
+            //начало транзакции фрагмента
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            //добавление секундомера и включение транзакции в стек возврата
+            ft.add(R.id.stopwatch_container, stopwatch);
+            ft.addToBackStack(null);
+            //включение переходной анимации для транзакции
+            ft.setTransition(android.support.v4.app.FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            //закрепление транзакции
+            ft.commit();
+        }
     }
 }
